969865cb9166593ef9cad49ef1071fe1607cb8c9
ADITYA GOJA <79311043+adi27398@users.noreply.github.com>
2024-07-09 02:15:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from "react";
+
+class App extends Component{
+  name="cipherSchools";
+  render() {
+    return (
+      <div>
+        <h1>This is {this.name}!</h1>
+      </div>
+      
+    );
+   
+
+   
+  }
+}
+
+export default App;
